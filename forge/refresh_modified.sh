1722a6b02a4d26a09f988bcbb2769cc82b04fc8f
Bharat Mediratta <bharat@menalto.com>
2008-12-15 09:20:58 +0000

    Generate the modified dir properly.

--- forge/refresh_modified.sh
+++ forge/refresh_modified.sh
@@ -3,3 +3,4 @@ find modified -name .svn -prune -o -type f -print0 | xargs -0 rm
 tar cf - --exclude='.svn' upstream | (cd modified && tar -xf - --strip-components 1)
 
 # Remove all the stuff we don't want in Gallery3
+rm -r modified/i18n
