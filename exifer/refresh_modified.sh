b5274d260c47667d411a680a09ab80197ed29549
Bharat Mediratta <bharat@menalto.com>
2009-02-07 22:06:17 +0000

    Add our preamble to all exifer files

--- exifer/refresh_modified.sh
+++ exifer/refresh_modified.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 find modified -name .svn -prune -o -type f -print0 | xargs -0 rm
 tar cf - --exclude='.svn' upstream | (cd modified && tar -xf - --strip-components 1)
+for file in `find modified -name .svn -prune -o \( -name "*.php" -print \)`; do
+  perl -pi -e 'print "<?php defined(\"SYSPATH\") or die(\"No direct script access.\"); ?>\n" if ($. == 1)' $file
+done
 
-# Remove all the stuff we don't want in Gallery3
