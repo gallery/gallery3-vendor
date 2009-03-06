95702f35450c5230282d267cd6460310663ec916
Tim Almdal <tnalmdal@shaw.ca>
2009-03-06 17:03:37 +0000

    Add the tabs css to gallery3

--- ui.themeroller/refresh_modified.sh
+++ ui.themeroller/refresh_modified.sh
@@ -1,6 +1,6 @@
 find modified -name .svn -prune -o -type f -print0 | xargs -0 rm
 cp /dev/null modified/ui.base.css
-for css in core theme datepicker dialog progressbar resizable; do
+for css in core theme datepicker dialog progressbar resizable tabs; do
     unzip -q -o -j upstream/jquery-ui-themeroller.zip -d modified jquery-ui-themeroller/theme/ui.$css.css
     echo "@import \"ui.$css.css\";" >> modified/ui.base.css
 done
