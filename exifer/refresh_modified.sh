4ee4c599571d7fa8d17ac6507495a55e22cc278a
Bharat Mediratta <bharat@menalto.com>
2009-04-28 03:23:54 +0000

    Strip carriage returns from upstream files

--- exifer/refresh_modified.sh
+++ exifer/refresh_modified.sh
@@ -3,5 +3,6 @@ find modified -name .svn -prune -o -type f -print0 | xargs -0 rm
 tar cf - --exclude='.svn' upstream | (cd modified && tar -xf - --strip-components 1)
 for file in `find modified -name .svn -prune -o \( -name "*.php" -print \)`; do
   perl -pi -e 'print "<?php defined(\"SYSPATH\") or die(\"No direct script access.\"); ?>\n" if ($. == 1)' $file
+  perl -pi -e 's/\r$//' $file
 done
 
