deed0f84f17145956bcfd20b698b0d2c81f520a7
Bharat Mediratta <bharat@menalto.com>
2009-05-18 01:09:08 +0000

    Drop unnecesary dirs, add preamble to whatever's left

--- forge/refresh_modified.sh
+++ forge/refresh_modified.sh
@@ -2,5 +2,14 @@
 find modified -name .svn -prune -o -type f -print0 | xargs -0 rm
 tar cf - --exclude='.svn' upstream | (cd modified && tar -xf - --strip-components 1)
 
+for file in `find modified -name .svn -prune -o \( -name "*.php" -print \)`; do
+  perl -pi -e '$_ = "<?php defined(\"SYSPATH\") or die(\"No direct script access.\"); \n" if ($. == 1)' $file
+done
+
 # Remove all the stuff we don't want in Gallery3
 rm -r modified/i18n
+rm -r modified/controllers
+rm -r modified/models
+rm -r modified/views
+
+
