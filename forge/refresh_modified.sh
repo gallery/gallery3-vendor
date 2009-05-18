1325ae8ed3ce1af07bc0b29075055f0199244718
Bharat Mediratta <bharat@menalto.com>
2009-05-18 04:01:15 +0000

    Get rid of trailing whitespace

--- forge/refresh_modified.sh
+++ forge/refresh_modified.sh
@@ -3,7 +3,7 @@ find modified -name .svn -prune -o -type f -print0 | xargs -0 rm
 tar cf - --exclude='.svn' upstream | (cd modified && tar -xf - --strip-components 1)
 
 for file in `find modified -name .svn -prune -o \( -name "*.php" -print \)`; do
-  perl -pi -e '$_ = "<?php defined(\"SYSPATH\") or die(\"No direct script access.\"); \n" if ($. == 1)' $file
+  perl -pi -e '$_ = "<?php defined(\"SYSPATH\") or die(\"No direct script access.\");\n" if ($. == 1)' $file
 done
 
 # Remove all the stuff we don't want in Gallery3
