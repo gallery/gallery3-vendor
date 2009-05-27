b462998d7e1a7eef5beed1cde841739ab6315394
Bharat Mediratta <bharat@menalto.com>
2009-05-26 22:18:41 -0700

    Updated instructions for Git.

--- exifer/refresh_modified.sh
+++ exifer/refresh_modified.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
-find modified -name .svn -prune -o -type f -print0 | xargs -0 rm
-tar cf - --exclude='.svn' upstream | (cd modified && tar -xf - --strip-components 1)
-for file in `find modified -name .svn -prune -o \( -name "*.php" -print \)`; do
+rm -rf modified
+rsync -ra upstream/ modified/
+
+for file in `find modified -name "*.php"`; do
   perl -pi -e '$_ = "<?php defined(\"SYSPATH\") or die(\"No direct script access.\");\n" if ($. == 1)' $file
   perl -pi -e 's/\r$//' $file
 done
--- exifer/refresh_upstream.sh
+++ exifer/refresh_upstream.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-find upstream -name .svn -prune -o -type f -print0 | xargs -0 rm
+rm -rf upstream
 svn export --force http://www.zenphoto.org/svn/trunk/zp-core/exif/ upstream
+git add upstream
