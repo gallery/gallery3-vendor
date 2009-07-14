b149cd2d6d990c1d1cf716253c541f266e6604fe
Tim Almdal <tnalmdal@shaw.ca>
2009-07-14 07:47:17 -0700

    Update to the release code as version 4.0.0

--- htmlpurifier/refresh_modified.sh
+++ htmlpurifier/refresh_modified.sh
@@ -3,32 +3,9 @@ rm -rf modified
 rsync -ra upstream/ modified/
 
 # Remove all the stuff we don't want in Gallery3
-rm -r modified/art
-rm -r modified/extras
-rm -r modified/maintenance
-rm -r modified/benchmarks
-rm -r modified/configdoc
-rm -r modified/smoketests
-rm -r modified/tests
-rm -r modified/docs
-rm -r modified/plugins
-
-rm modified/release1-update.php
-rm modified/VERSION
-rm modified/FOCUS
 rm modified/NEWS
-rm modified/release2-tag.php
-rm modified/WHATSNEW
 rm modified/INSTALL
-rm modified/package.php
-rm modified/WYSIWYG
 rm modified/CREDITS
-rm modified/INSTALL.fr.utf8
-rm modified/phpdoc.ini
-rm modified/test-settings.sample.php
-rm modified/Doxyfile
-rm modified/README
-rm modified/TODO
 
 cd modified/library
 mv * ..
@@ -43,3 +20,4 @@ for file in `find modified -name "*.php"`; do
   perl -pi -e 's/\r$//' $file
 done
 
+git add modified
