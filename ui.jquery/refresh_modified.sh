105a66c02c088d14aa5b8b78456e31eaf9042eb8
Bharat Mediratta <bharat@menalto.com>
2009-09-11 10:15:58 -0700

    Change the way we deal with jquery-ui such that we pull down the raw JS, give ourselves an opportunity patch it, then minify the result using jsmin-php.
    This reverts the work of modifying selectable, and switches us back to
    using selectable from 1.7.2 which is easier to maintain.  If we can't
    get that version of selectable to work we'll have to do something more
    drastic.

--- ui.jquery/refresh_modified.sh
+++ ui.jquery/refresh_modified.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-cat upstream/*.js | perl -pe 's/\r//g' > modified/jquery-ui.js
+rm modified/*
+unzip -d modified -j upstream/*.zip 'development-bundle/ui/jquery-ui-*.custom.js'
+mv modified/jquery-ui-*.custom.js modified/jquery-ui.js
+
+# Apply patches here, eg:
+#   patch modified/jquery-ui.js < patches/ticket_4850.txt
+
+# Minify the JS
+php -r 'require "../jsmin-php/upstream/jsmin-1.1.1.php"; echo JSMin::minify(file_get_contents("modified/jquery-ui.js"));' > modified/jquery-ui-jsmin.js
+mv modified/jquery-ui-jsmin.js modified/jquery-ui.js
