e50e69c2562ea79e6767a9176a670fc332d4a821
Tim Almdal <tnalmdal@shaw.ca>
2009-09-12 08:25:00 -0700

    Minor changes to the readme and comments in refresh_modified.sh to reflect that we have no current patches to the jquery code. Removed the patch for the development version as we are now using the release version of Selectable

--- ui.jquery/refresh_modified.sh
+++ ui.jquery/refresh_modified.sh
@@ -4,7 +4,7 @@ unzip -d modified -j upstream/*.zip 'development-bundle/ui/jquery-ui-*.custom.js
 mv modified/jquery-ui-*.custom.js modified/jquery-ui.js
 
 # Apply patches here, eg:
-#   patch modified/jquery-ui.js < patches/ticket_4850.txt
+#   patch modified/jquery-ui.js < patches/ticket_xxxx.txt
 
 # Minify the JS
 php -r 'require "../jsmin-php/upstream/jsmin-1.1.1.php"; echo JSMin::minify(file_get_contents("modified/jquery-ui.js"));' > modified/jquery-ui-jsmin.js
