9e1dd029232c533651d943d8605c32e595ec4f88
Bharat Mediratta <bharat@menalto.com>
2009-09-17 22:04:59 -0700

    Get rid of create_unpacked.sh and replace it with a --no-minify argument to refresh_modified.sh

--- ui.jquery/refresh_modified.sh
+++ ui.jquery/refresh_modified.sh
@@ -9,6 +9,12 @@ patch modified/jquery-ui.js < patches/ticket_4441.txt
 patch modified/jquery-ui.js < patches/ticket_4377.txt
 patch modified/jquery-ui.js < patches/ticket_2843.txt
 
-# Minify the JS
-php -r 'require "../jsmin-php/upstream/jsmin-1.1.1.php"; echo JSMin::minify(file_get_contents("modified/jquery-ui.js"));' > modified/jquery-ui-jsmin.js
-mv modified/jquery-ui-jsmin.js modified/jquery-ui.js
+# Minify the JS (unless specifically asked not to)
+if [ "$1" == "--no-minify" ];
+then
+    echo "Not minifying!"
+else
+    echo "Minifying!"
+    php -r 'require "../jsmin-php/upstream/jsmin-1.1.1.php"; echo JSMin::minify(file_get_contents("modified/jquery-ui.js"));' > modified/jquery-ui-jsmin.js
+    mv modified/jquery-ui-jsmin.js modified/jquery-ui.js
+fi
