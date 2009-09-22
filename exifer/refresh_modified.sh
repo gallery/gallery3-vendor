35ca4fb6606d7eaf8c4d8dbe48056041a3038fe0
Tim Almdal <tnalmdal@shaw.ca>
2009-09-22 10:48:30 -0700

    Changed refresh_modified.sh to replaces all occurrences of 'gettext(' with '(string)t('.  Gallery3 doesn't use gettext and we don't check for it in our requirements.  This also has the advantage of allowing our translators to translate strings in the exifer. Fixes ticket 672.

--- exifer/refresh_modified.sh
+++ exifer/refresh_modified.sh
@@ -5,5 +5,6 @@ rsync -ra upstream/ modified/
 for file in `find modified -name "*.php"`; do
   perl -pi -e '$_ = "<?php defined(\"SYSPATH\") or die(\"No direct script access.\");\n" if ($. == 1)' $file
   perl -pi -e 's/\r$//' $file
+  perl -pi -e 's/gettext\(/\(string\) t\(/' $file
 done
 
