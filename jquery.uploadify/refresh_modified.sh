d9b96f3678b92ca5bc47269b697533550172416e
Tim Almdal <tnalmdal@shaw.ca>
2009-11-10 07:54:13 -0800

    Automate the changing of the queue width and converting CRLF's to LF's

--- jquery.uploadify/refresh_modified.sh
+++ jquery.uploadify/refresh_modified.sh
@@ -6,8 +6,16 @@ rm modified/jquery-1.3.2.min.js
 rm modified/jquery.uploadify.v2.1.0.js
 rm modified/dwsync.xml
 rm modified/swfobject.js
+rm modified/default.css
+rm modified/expressInstall.swf
 rm modified/*.as
 rm modified/*.pdf
 rm modified/*.php
 
 mv modified/jquery.uploadify.v2.1.0.min.js modified/jquery.uploadify.min.js
+
+# Set the width of the queue item to auto
+patch -p0 modified/uploadify.css < patches/auto_width.patch.txt
+
+# remove the carriage returns
+tr -d '\r' < modified/uploadify.css > modified/uploadify.css
