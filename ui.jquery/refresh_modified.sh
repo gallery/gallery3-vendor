63434982f16d68c160381e663b97cb14c2746633
Bharat Mediratta <bharat@menalto.com>
2012-06-24 11:42:24 -0700

    Updated to 1.8.21 and disabled all patches

--- ui.jquery/refresh_modified.sh
+++ ui.jquery/refresh_modified.sh
@@ -5,10 +5,15 @@ mv modified/jquery-ui-*.custom.js modified/jquery-ui.js
 
 # Apply patches here, eg:
 #   patch modified/jquery-ui.js < patches/ticket_xxxx.txt
-patch modified/jquery-ui.js < patches/ticket_4441.txt
-patch modified/jquery-ui.js < patches/ticket_4377.txt
-patch modified/jquery-ui.js < patches/ticket_2843.txt
-patch modified/jquery-ui.js < patches/ticket_5370.txt
+#
+# Note that since the upgrade to 1.8.21 I have commented out
+# these patches so that we can see if we need them before we
+# figure out how to apply them (not all of them apply cleanly anymore)
+#
+#patch modified/jquery-ui.js < patches/ticket_4441.txt
+#patch modified/jquery-ui.js < patches/ticket_4377.txt
+#patch modified/jquery-ui.js < patches/ticket_2843.txt
+#patch modified/jquery-ui.js < patches/ticket_5370.txt
 
 # Minify the JS (unless specifically asked not to)
 if [ "$1" == "--no-minify" ];
