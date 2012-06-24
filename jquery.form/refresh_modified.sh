e7d35447a26a9941f86dc8536b83b9c9d2402a8c
Bharat Mediratta <bharat@menalto.com>
2012-06-24 11:40:12 -0700

    Updated modified to 5904d38c2636bf037c8328aab6576361af53da75 and add jsmin support.

--- jquery.form/refresh_modified.sh
+++ jquery.form/refresh_modified.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
 cat upstream/jquery.form.js | perl -pe 's/\r//g' > modified/jquery.form.js
+
+php -r 'require "../jsmin-php/upstream/jsmin-1.1.1.php"; echo JSMin::minify(file_get_contents("modified/jquery.form.js"));' > modified/jquery.form-jsmin.js
+mv modified/jquery.form-jsmin.js modified/jquery.form.js
+
