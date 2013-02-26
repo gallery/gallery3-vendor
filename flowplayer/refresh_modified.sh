7061ba2a6e6c6b3cfae0b269c839b1a14b8f4f3e
shadlaws <shad@shadlaws.com>
2013-02-26 15:48:15 +0100

    #2017, 2019 - Update Flowplayer to 3.2.16, fix special character issues.

--- flowplayer/refresh_modified.sh
+++ flowplayer/refresh_modified.sh
@@ -1,15 +1,26 @@
 #!/bin/bash
+FP_VERSION="3\.2\.12"
+
 rm modified/*
 
-# Tack on iPad support
-cat upstream/flowplayer-src/*.js > modified/flowplayer.js
-cat upstream/ipad/flowplayer.*.js >> modified/flowplayer.js
+# Generate patched flashembed.js
+cp upstream/flashembed.js modified/flashembed.js
+patch modified/flashembed.js < patches/ticket_30_rev20130226.txt
+
+# Add version number to flowplayer.js
+cp upstream/flowplayer-src.js modified/flowplayer.js
+sed -i "s/@VERSION/"$FP_VERSION"/g" modified/flowplayer.js
+
+# Rebuild flowplayer.js, tack on iPad support
+cat modified/flashembed.js >> modified/flowplayer.js
+cat upstream/flowplayer.ipad-[0-9]*.js >> modified/flowplayer.js
+unlink modified/flashembed.js
 
 # Stage the SWF files
 cp upstream/flowplayer.pseudostreaming/flowplayer.pseudostreaming-byterange-[0-9]*.swf modified/flowplayer.pseudostreaming-byterange.swf
 cp upstream/flowplayer.pseudostreaming/flowplayer.pseudostreaming-[0-9]*.swf modified/flowplayer.pseudostreaming.swf
-cp upstream/flowplayer-packaged/flowplayer.controls-3.2.12.swf modified/flowplayer.controls.swf
-cp upstream/flowplayer-packaged/flowplayer-3.2.12.swf modified/flowplayer.swf
+cp upstream/flowplayer/flowplayer.controls-[0-9]*.swf modified/flowplayer.controls.swf
+cp upstream/flowplayer/flowplayer-[0-9]*.swf modified/flowplayer.swf
 
 # Minify the JS (unless specifically asked not to)
 if [ "$1" == "--no-minify" ];
