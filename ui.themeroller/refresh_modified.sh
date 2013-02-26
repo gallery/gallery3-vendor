c1d0493409c14a217c540222baf8f0c9015c0c12
shadlaws <shad@shadlaws.com>
2013-02-26 16:37:41 +0100

    Update to latest Themeroller. - updated to v1.10.1-compatible Themeroller. - removed refresh script as no special files need to be generated (previously shipped with several CSS files, now ships with only one unified file)

--- ui.themeroller/refresh_modified.sh
+++
@@ -1,9 +0,0 @@
-rm -rf modified
-mkdir modified
-cp /dev/null modified/ui.base.css
-for css in core theme datepicker dialog progressbar resizable tabs; do
-    unzip -q -o -j upstream/jquery-ui-themeroller.zip -d modified jquery-ui-themeroller/theme/ui.$css.css
-    echo "@import \"ui.$css.css\";" >> modified/ui.base.css
-done
-
-unzip -q -o -j upstream/jquery-ui-themeroller.zip -d modified/images jquery-ui-themeroller/theme/images/\*
