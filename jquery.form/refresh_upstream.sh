415bf5a3e15e7b831e3cb4327f6f6a3563b2f06b
shadlaws <shad@shadlaws.com>
2013-02-26 11:32:18 +0100

    Update jQuery and all associated libraries to current as of 2013/02/26. - updated the 6 we use, removed the 3 we no longer need. - created README to document everything. - removed all patches. - removed all refresh scripts (all of them are direct downloads). - kept legacy patches in subdirectory for reference purposes.

--- jquery.form/refresh_upstream.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-git clone -q git://github.com/malsup/form.git tmp
-cp tmp/jquery.form.js upstream/jquery.form.js
-REV=$(cd tmp && git log | head -1 | awk '{print $2}')
-rm -rf tmp
-echo "git commit -m 'Updated upstream to $REV' upstream"
-
--- jquery.scrollTo/refresh_modified.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-cat upstream/jquery.scrollTo.js | perl -pe 's/\r//g' > modified/jquery.scrollTo.js
