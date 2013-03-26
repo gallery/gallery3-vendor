e5d302828959601283b36c0a22f9fe72f055d0c5
shadlaws <shad@shadlaws.com>
2013-03-26 09:44:05 +0100

    Update to PHPass v0.3.

--- phpass/refresh_upstream.sh
+++ phpass/refresh_upstream.sh
@@ -1,6 +1,6 @@
-PACKAGE=http://www.openwall.com/phpass/phpass-0.1.tar.gz
+PACKAGE=http://www.openwall.com/phpass/phpass-0.3.tar.gz
 
 rm -rf upstream
 mkdir upstream
 wget -q -O- $PACKAGE | (cd upstream && tar xzf - --strip-components=1)
-git add upstream
+# git add upstream
