f3d81fb5a5b5621b037f61084390aea152fb177b
Bharat Mediratta <bharat@menalto.com>
2013-04-26 00:51:13 -0400

    Switch to the 3.3/develop branch.

--- kohana3/refresh_upstream.sh
+++ kohana3/refresh_upstream.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 rm -rf upstream
-git clone --recurse-submodules http://github.com/kohana/kohana.git upstream
+git clone -b 3.3/develop --recurse-submodules http://github.com/kohana/kohana.git upstream
 rm -rf `find upstream -name '.git*'`
