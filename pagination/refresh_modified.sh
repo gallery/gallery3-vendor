72c3226dc45c3dbfee170cbec427e1d35f899a81
Bharat Mediratta <bharat@menalto.com>
2013-05-01 11:32:24 -0400

    Tweak refresh_modified to remove windows style line endings Update the README to change references to Formo to Pagination.

--- pagination/refresh_modified.sh
+++ pagination/refresh_modified.sh
@@ -1,6 +1,7 @@
-#!/bin/sh
+#!bin/sh
 rm -rf modified
 rsync -ra upstream/ modified/
+perl -pi -e 's/\r$//' `find modified -name '*.php'`
 
 # Remove all the stuff we don't want in Gallery3.
 # Note that this also prunes the default config since Gallery3 uses its own.
